5c7942b3126b1b56d0365b9260ecf990a5780ec2
rhinejoel <joelrhine7@gmail.com>
2024-10-09 13:01:03 -0400

    Contain and deploy app

--- yt-web-client/app/page.tsx
+++ yt-web-client/app/page.tsx
@@ -11,12 +11,15 @@ export default async function Home() {
 		<main>
 		{
 			videos.map((video) => (
-			<Link href={`/watch?v=${video.filename}`}>
+			<Link href={`/watch?v=${video.filename}`} key={video.id}>
 				<Image src={'/thumbnail.png'} alt='video' width={120} height={80}
-				className={styles.thumbnail}/>
+					className={styles.thumbnail}/>
 			</Link>
+			  
 			))
 		}
 		</main>
 	)
 }
+
+export const revalidate = 30;
